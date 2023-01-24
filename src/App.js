c569578afde70a5394749a50d1104a1e19e9a02a
takumi <hal.bandotakumi@gmail.com>
2023-01-25 00:30:53 +0900

    activity portfolio

--- src/App.js
+++ src/App.js
@@ -82,7 +82,7 @@ function App() {
             <h2>My profile</h2>
             <ul>
               <li>Name: 坂東　拓実</li>
-              <li>Age: 18</li>
+              <li>Age: 19</li>
               <li>Hometown: 徳島</li>
               <li>
                 hobie: 音楽を聴く(主にhip hop), バイクに乗る,
@@ -107,63 +107,46 @@ function App() {
           </div>
         </section>
         <hr className="m-0" />
-        {/* Experience*/}
-        <section className="resume-section" id="experience">
+      
+
+        
+       
+        {/* Interests*/}
+        <section className="resume-section" id="interests">
           <div className="resume-section-content">
-            <h2 className="mb-5">Hometown</h2>
-            <div className="d-flex flex-column flex-md-row justify-content-between mb-5">
-              <div className="flex-grow-1">
-                <h3 className="mb-0">徳島紹介</h3>
-                <div className="subheading mb-3">阿波踊り</div>
-                <img
-                  className="w-50 p-3"
-                  src={awa}
-                />
-                <p>
-                  徳島市の阿波踊りは毎年8月12日から15日まで4日間の開催となり、
-                  人出は約130万人、踊り子は約10万人が繰り出します。
-                  期間中は、徳島市中心街一円が踊りの渦に巻き込まれ、興奮のるつぼと化します。
-                  ちなみに、徳島の小学生は低学年の時に阿波踊りを踊らされます。
-                </p>
-              </div>
-            </div>
-            <div className="d-flex flex-column flex-md-row justify-content-between mb-5">
-              <div className="flex-grow-1">
-                <div className="subheading mb-3">スダチ</div>
-                <img className="w-50 p-3" src={sudachi} />
-                <p>
-                  香りがよく上品な味がするそうです。魚などにかけて使うことが多いです。
-                  僕はゆず派です。
-                </p>
-              </div>
-            </div>
-            <div className="d-flex flex-column flex-md-row justify-content-between mb-5">
-              <div className="flex-grow-1">
-                <div className="subheading mb-3">徳島ラーメン</div>
-                <img
-                  className="w-50 p-3"
-                  src={ramen}
-                />
-                <p>
-                  味は豚骨醤油ですが、地域によって変わったり店によって変わったりするので
-                  県民は自分好みの店を見つけています。
-                </p>
-              </div>
-            </div>
-            <div className="d-flex flex-column flex-md-row justify-content-between">
-              <div className="flex-grow-1">
-                <div className="subheading mb-3">旧吉野川</div>
-                <img className="w-50 p-3" src={fishing}/>
-                <p>
-                  これは、旧吉野川で釣った時の写真です。
-                  日本のメジャーなバス釣りスポットで全国から有名なバスプロが来たり取材や大きい大会が開かれています。
-                  ぜひ徳島に来るときは釣り竿を持ってきてください^^
-                </p>
-              </div>
-            </div>
+            <h2 className="mb-5">Portfolio</h2>
+            <ol>
+              <li>位置情報共有アプリ(現在作業休止中)</li>
+              <p>
+                （詳細）
+                ゼンリーのようにマップで居場所を表示させるのではなく、あくまで今何しているかだけを表示させる。
+                半径500mぐらいに友達が居れば通知が行く。チャットアプリもつける。
+              </p>
+              <li>(仮想idea) 個人業者の引っ越しマッチングアプリ</li>
+              <p>
+                業者に頼むと高くなりがちだが、個人の業者に頼むことで安く抑えることができる。
+                ですがこのサービスを作るにはケガや家具家電の故障したときどうするかなど様々な問題点があるので手が付けれていません
+              </p>
+              
+
+
+              <li>チャットアプリ</li>
+              <p>Udemyの講座を参考にしてチャットアプリを作成しました。react javascript firebase muiを用いて開発しました。</p>
+              <img src="" alt="" className="w-50 p-3"></img>
+              <link rel=""></link>
+
+
+
+              <li>技育博</li>
+              <img src="/IMG_4865.jpg" className="w-50 p-3" alt=""></img>
+              <p>
+                サークルで技育博に参加しました。制作中の作品をプレゼンし多くの方からのフィードバックを得ていい経験になりました。
+              </p>
+            </ol>
           </div>
         </section>
-        <hr className="m-0" />
+        
+
         {/* Education*/}
         <section className="resume-section" id="education">
           <div className="resume-section-content">
@@ -189,8 +172,60 @@ function App() {
           </div>
         </section>
         <hr className="m-0" />
-        {/* Skills*/}
-        <section className="resume-section" id="skills">
+        {/* Awards*/}
+          {/* Experience*/}
+          <section className="resume-section" id="experience">
+          <div className="resume-section-content">
+            <h2 className="mb-5">Hometown</h2>
+            <div className="d-flex flex-column flex-md-row justify-content-between mb-5">
+              <div className="flex-grow-1">
+                <h3 className="mb-0">徳島紹介</h3>
+                <div className="subheading mb-3">阿波踊り</div>
+                <img className="w-50 p-3" src={awa} />
+                <p>
+                  徳島市の阿波踊りは毎年8月12日から15日まで4日間の開催となり、
+                  人出は約130万人、踊り子は約10万人が繰り出します。
+                  期間中は、徳島市中心街一円が踊りの渦に巻き込まれ、興奮のるつぼと化します。
+                  ちなみに、徳島の小学生は低学年の時に阿波踊りを踊らされます。
+                </p>
+              </div>
+            </div>
+            <div className="d-flex flex-column flex-md-row justify-content-between mb-5">
+              <div className="flex-grow-1">
+                <div className="subheading mb-3">スダチ</div>
+                <img className="w-50 p-3" src={sudachi} />
+                <p>
+                  香りがよく上品な味がするそうです。魚などにかけて使うことが多いです。
+                  僕はゆず派です。
+                </p>
+              </div>
+            </div>
+            <div className="d-flex flex-column flex-md-row justify-content-between mb-5">
+              <div className="flex-grow-1">
+                <div className="subheading mb-3">徳島ラーメン</div>
+                <img className="w-50 p-3" src={ramen} />
+                <p>
+                  味は豚骨醤油ですが、地域によって変わったり店によって変わったりするので
+                  県民は自分好みの店を見つけています。
+                </p>
+              </div>
+            </div>
+            <div className="d-flex flex-column flex-md-row justify-content-between">
+              <div className="flex-grow-1">
+                <div className="subheading mb-3">旧吉野川</div>
+                <img className="w-50 p-3" src={fishing} />
+                <p>
+                  これは、旧吉野川で釣った時の写真です。
+                  日本のメジャーなバス釣りスポットで全国から有名なバスプロが来たり取材や大きい大会が開かれています。
+                  ぜひ徳島に来るときは釣り竿を持ってきてください^^
+                </p>
+              </div>
+            </div>
+          </div>
+        </section>
+        <hr className="m-0" />
+         {/* Skills*/}
+         <section className="resume-section" id="skills">
           <div className="resume-section-content">
             <h2 className="mb-5">Skills</h2>
             <div className="subheading mb-3">
@@ -219,27 +254,6 @@ function App() {
           </div>
         </section>
         <hr className="m-0" />
-        {/* Interests*/}
-        <section className="resume-section" id="interests">
-          <div className="resume-section-content">
-            <h2 className="mb-5">Idea</h2>
-            <ol>
-              <li>位置情報共有アプリ(現在進行中)</li>
-              <p>
-                （詳細）
-                ゼンリーのようにマップで居場所を表示させるのではなく、あくまで今何しているかだけを表示させる。
-                半径500mぐらいに友達が居れば通知が行く。チャットアプリもつける。
-              </p>
-              <li>(仮想idea) 個人業者の引っ越しマッチングアプリ</li>
-              <p>
-                業者に頼むと高くなりがちだが、個人の業者に頼むことで安く抑えることができる。
-                ですがこのサービスを作るにはケガや家具家電の故障したときどうするかなど様々な問題点があるので手が付けれていません
-              </p>
-            </ol>
-          </div>
-        </section>
-        <hr className="m-0" />
-        {/* Awards*/}
       </div>
     </div>
   );
